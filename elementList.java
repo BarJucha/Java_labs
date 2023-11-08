0adf07aaee6095b2ab97e7b539266ec59d7d3ecf
BarJucha <115100213+BarJucha@users.noreply.github.com>
2023-11-08 03:40:06 +0100

    Add files via upload

---
+++ elementList.java
@@ -0,0 +1,4 @@
+public class elementList<E> {
+    public E data = null;
+    public elementList<E> next = null;
+}
---
+++ listIterator.java
@@ -0,0 +1,8 @@
+public class listIterator <E>{
+    private elementList<E> current;
+    private int index;
+    public listIterator(elementList<E> e){
+        this.current = e;
+
+    }
+}
